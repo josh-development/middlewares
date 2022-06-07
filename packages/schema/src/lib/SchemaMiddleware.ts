7ff49a9fcda17798112dd6d2baf0f7cf7dcf8823
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-06-07 00:01:07 -0400

    feat(SchemaMiddleware): add the `schema` option property

--- packages/schema/src/lib/SchemaMiddleware.ts
+++ packages/schema/src/lib/SchemaMiddleware.ts
@@ -1,8 +1,11 @@
 import { ApplyMiddlewareOptions, Middleware } from '@joshdb/middleware';
+import type { BaseValidator } from '@sapphire/shapeshift';
 
 @ApplyMiddlewareOptions({ name: 'schema' })
-export class SchemaMiddleware<StoredValue = unknown> extends Middleware<SchemaMiddleware.ContextData, StoredValue> {}
+export class SchemaMiddleware<StoredValue = unknown> extends Middleware<SchemaMiddleware.ContextData<StoredValue>, StoredValue> {}
 
 export namespace SchemaMiddleware {
-  export interface ContextData {}
+  export interface ContextData<StoredValue = unknown> {
+    schema: BaseValidator<StoredValue>;
+  }
 }
