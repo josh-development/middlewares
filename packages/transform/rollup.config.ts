26e7cac0b2acbd9467ae1ec8a3111dc49263c4b9
c0reme <cameron.huntley87@gmail.com>
2022-10-07 15:36:55 +0100

    chore(transform): remove umd

--- packages/transform/rollup.config.ts
+++ packages/transform/rollup.config.ts
@@ -16,16 +16,6 @@ export default {
       format: 'es',
       exports: 'named',
       sourcemap: true
-    },
-    {
-      file: './dist/index.umd.js',
-      format: 'umd',
-      name: 'Transform',
-      sourcemap: true,
-      globals: {
-        '@joshdb/provider': 'JoshProvider',
-        '@sapphire/utilities': 'SapphireUtilities'
-      }
     }
   ],
   external: ['@joshdb/provider', '@sapphire/utilities'],
